46a92faccb4893a84a26bede7736ebf5382c47c2
tejad245 <tejad245@gmail.com>
2019-06-11 20:31:32 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function(){
+  "use strict";
+$('#slider-carousel').caroufredsel({
+  responsive:true,
+  width:'100%',
+  circular:true,
+  scroll:{
+    items:1,
+    duration:500,
+    pauseOnHover:true
+  },
+  auto:true,
+  items:
+  {
+    visible:
+    {
+      min:1,
+      max:1
+    },
+    heigh:"variable"
+  },
+  pagination:{
+    container:".sliderpager",
+    pageAnchorBuilder:false
+  }
+
+})
+
+
+
+});
